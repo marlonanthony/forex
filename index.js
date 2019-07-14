16ad25119d802d939cbc20b865a1b1c5768bb12f
marlonanthony <mad1083@gmail.com>
2019-07-14 06:14:43 -0400

    connected to database

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('express')()
 const { ApolloServer } = require('apollo-server-express')
+const mongoose = require('mongoose')
 
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers') 
 const CurrencyAPI = require('./datasources/currencies')
+const { mongoPassword } = require('./config/keys')
 
 const server = new ApolloServer({ 
   typeDefs,
@@ -15,8 +17,8 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app })
 
-const PORT = 4000
-
-app.listen(PORT, () => {
+mongoose
+.connect(`mongodb+srv://marlon:${mongoPassword}@cluster0-o028g.mongodb.net/forex?retryWrites=true&w=majority`, { useNewUrlParser: true })
+.then(() => app.listen(4000, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-})
+})).catch(err => console.log(err)) 
