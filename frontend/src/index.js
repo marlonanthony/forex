5d2d3524b71a7198799b891a123ff1a06ba2bb26
marlonanthony <mad1083@gmail.com>
2019-07-18 20:33:33 -0400

    added index.js, App.js, and Landing.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { ApolloClient } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory' 
+import { HttpLink } from 'apollo-link-http'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
+import './index.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = new InMemoryCache() 
+const client = new ApolloClient({
+    cache,
+    link: new HttpLink({
+        uri: 'http://localhost:4000/graphql',
+        credentials: 'include' 
+    })
+})
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </BrowserRouter>, document.getElementById('root')
+)
