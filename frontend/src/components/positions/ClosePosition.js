3092033915569225fc8d4bafd7900afa923108ef
marlonanthony <mad1083@gmail.com>
2019-07-23 23:13:35 -0400

    refactored Pair.js. Created ClosePosition.js

---
+++ frontend/src/components/positions/ClosePosition.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { Mutation } from 'react-apollo'
+import { Link } from 'react-router-dom'
+
+import { CLOSEPOSITION } from '../../graphql/mutations/closePosition'
+import { MEQUERY } from '../../graphql/queries/me'
+
+export default function ClosePosition({ id, bidPrice, askPrice, position }) {
+  const [showModal, setShowModal] = useState(false)
+
+  return (
+    <Mutation 
+      mutation={CLOSEPOSITION} 
+      variables={ position === 'long' 
+        ? { id, closedAt: +bidPrice } 
+        : { id, closedAt: +askPrice } }
+      refetchQueries={[{ query: MEQUERY }]}
+    >
+      {(closePosition, { data, loading, error }) => {
+        if(loading) return <div>Loading...</div>
+        if(error) return <p>{ error.message }</p>
+        return closePosition && (
+          <>
+            <button onClick={() => {
+              position == 'long' 
+                ? alert('Are you sure you want to sell your long position?') 
+                : alert('Are you sure you want to close your short position?')
+              closePosition()
+              setShowModal(true)
+            }}>
+              { position === 'long' ? 'Sell' : 'Buy' }
+            </button> 
+            { data && data.closePosition.message && showModal && ( 
+              <div className='open_position_modal'>
+                <button onClick={() => setShowModal(false)}>X</button>
+                <p>{data && data.closePosition.message}!</p>
+                <Link to='/account'><span>Account</span></Link>
+              </div>
+            )}
+          </>
+        )
+      }}
+    </Mutation> 
+  )
+}
--- frontend/src/pages/Pair.js
+++ frontend/src/pages/Pair.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react'
-import { Mutation, Query } from 'react-apollo'
-import { Link, Redirect } from 'react-router-dom'
+import React from 'react'
+import { Query } from 'react-apollo'
+import { Redirect } from 'react-router-dom'
 
-import { CLOSEPOSITION } from '../graphql/mutations/closePosition'
 import { CURRENCY_PAIR_INFO } from '../graphql/queries/currencyPairInfo'
-import { MEQUERY } from '../graphql/queries/me'
+import ClosePosition from '../components/positions/ClosePosition'
 
 const Pair = props => {
-  const [showModal, setShowModal] = useState(false)
-
   if(!props.location.state) return <Redirect to='/login' />
   else {
     const {createdAt, lotSize, openedAt, pair, position, id } = props.location.state.pair,
@@ -33,38 +30,12 @@ const Pair = props => {
                 <p>{ name } your available balance: { bankroll.toLocaleString() +'.00' }</p> 
                 <div>
                   <button onClick={() => refetch()}>Refresh</button>
-                  <Mutation 
-                    mutation={CLOSEPOSITION} 
-                    variables={ position === 'long' 
-                      ? { id, closedAt: +bidPrice } 
-                      : { id, closedAt: +askPrice } }
-                    refetchQueries={[{ query: MEQUERY }]}
-                  >
-                    {(closePosition, { data, loading, error }) => {
-                      if(loading) return <div>Loading...</div>
-                      if(error) return <p>{ error.message }</p>
-                      return closePosition && (
-                        <>
-                          <button onClick={() => {
-                            position == 'long' 
-                            ? alert('Are you sure you want to sell your long position?') 
-                            : alert('Are you sure you want to close your short position?')
-                            closePosition()
-                            setShowModal(true) 
-                          }}>
-                            { position === 'long' ? 'Sell' : 'Buy' }
-                          </button> 
-                          { data && data.closePosition.message && showModal && ( 
-                            <div className='open_position_modal'>
-                              <button onClick={() => setShowModal(false)}>X</button>
-                              <p>{data && data.closePosition.message}!</p>
-                              <Link to='/account'><span>Account</span></Link>
-                            </div>
-                          )}
-                        </>
-                      )
-                    }}
-                  </Mutation> 
+                  <ClosePosition 
+                    id={id} 
+                    bidPrice={bidPrice} 
+                    askPrice={askPrice} 
+                    position={position} 
+                  />
                 </div>
               </div>
               <div>
@@ -83,8 +54,8 @@ const Pair = props => {
                       <p><span>Potential PL: </span>
                         {potentialProfitLossLong.toLocaleString()+'.00'}
                       </p>
-                    </>
-                  ) : (
+                    </> ) 
+                  : (
                     <>
                       <br />
                       <p><span>Current Ask Price: </span>{+askPrice}</p>
