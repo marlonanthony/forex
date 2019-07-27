60a1e65305c193f5cf8c3e2409fb752f15029ae8
marlonanthony <mad1083@gmail.com>
2019-07-26 22:49:00 -0400

    refactored SelectList.js

--- frontend/src/components/SelectList.js
+++ frontend/src/components/SelectList.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-export default function SelectList({ fc, setFc, tc, setTc }) {
-  return (
-    <select
-      value={`${fc}/${tc}`}
-      onChange={e => {
-          const [fromCurrency, toCurrency] = e.target.value.split('/')
-          setFc(fromCurrency)
-          setTc(toCurrency)
-      }}>
-      <option>EUR/USD</option>
-      <option>USD/JPY</option>
-      <option>GBP/USD</option>
-      <option>AUD/USD</option>
-      <option>USD/CHF</option>
-      <option>NZD/USD</option>
-      <option>USD/CAD</option>
-    </select>
-  )
-}
+const SelectList = ({ fc, setFc, tc, setTc }) => (
+  <select
+    value={`${fc}/${tc}`}
+    onChange={e => {
+      const [fromCurrency, toCurrency] = e.target.value.split('/')
+      setFc(fromCurrency)
+      setTc(toCurrency)
+    }}>
+    <option>EUR/USD</option>
+    <option>USD/JPY</option>
+    <option>GBP/USD</option>
+    <option>AUD/USD</option>
+    <option>USD/CHF</option>
+    <option>NZD/USD</option>
+    <option>USD/CAD</option>
+  </select>
+)
+
+export default SelectList
