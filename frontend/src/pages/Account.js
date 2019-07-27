d61a6bcacb760b4d1dd3f90e3f33ccc09a04d82c
marlonanthony <mad1083@gmail.com>
2019-07-26 23:56:02 -0400

    refactored Account.js

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -27,7 +27,7 @@ const Account = props => {
         <main>
           <h2>{data.me.name}</h2>
           <div>
-            <p><span>Available Balance: </span>{ data.me.bankroll.toLocaleString()}.00</p> 
+            <p><span>Available Balance: </span>{ data.me.bankroll.toLocaleString() }.00</p> 
             <p><span>Total P/L: </span>{count}</p>
             <AddFunds />
         </div>
@@ -36,10 +36,10 @@ const Account = props => {
           <div>
             <h3>New Position</h3>
             <div className='pair_divs'>
-              <p><span>Pair: </span>{props.location.state.data.openPosition.pair.pair}</p>
-              <p><span>Lot Size: </span>{props.location.state.data.openPosition.pair.lotSize.toLocaleString()}.00</p>
-              <p><span>Pip Dif: </span>{props.location.state.data.openPosition.pair.openedAt}</p>
-              <p><span>Position: </span>{props.location.state.data.openPosition.pair.position}</p>
+              <p><span>Pair: </span>{ props.location.state.data.openPosition.pair.pair }</p>
+              <p><span>Lot Size: </span>{ props.location.state.data.openPosition.pair.lotSize.toLocaleString() }.00</p>
+              <p><span>Pip Dif: </span>{ props.location.state.data.openPosition.pair.openedAt }</p>
+              <p><span>Position: </span>{ props.location.state.data.openPosition.pair.position }</p>
             </div>
           </div>
         )}
@@ -51,8 +51,8 @@ const Account = props => {
           { data.me.pairs && data.me.pairs.map(pair => pair.open && open && (
             <div className='pair_divs' key={pair.id}>
               <Link to={{ pathname: '/pair', state: { pair, me: data.me } }}>
-                { pair.pair && <p><span>Currency Pair: </span>{pair.pair}</p> }
-                { pair.lotSize && <p><span>Lot Size: </span>{pair.lotSize.toLocaleString()}.00</p> }
+                { pair.pair && <p><span>Currency Pair: </span>{ pair.pair }</p> }
+                { pair.lotSize && <p><span>Lot Size: </span>{ pair.lotSize.toLocaleString() }.00</p> }
                 { pair.position && <p><span>Position: </span>{ pair.position }</p> }
                 { pair.openedAt && <p><span>Opened At: </span>{ pair.openedAt.toFixed(4) }</p> }
                 { pair.createdAt && <p><span>Created At: </span>{ new Date(+pair.createdAt).toLocaleString() }</p> }
@@ -63,8 +63,8 @@ const Account = props => {
           { data.me.pairs && data.me.pairs.map(pair => !pair.open && !open && (
             <div className='pair_divs' key={pair.id}>
               <div>
-                { pair.pair && <p><span>Currency Pair: </span>{pair.pair}</p> }
-                { pair.lotSize && <p><span>Lot Size: </span>{pair.lotSize.toLocaleString()}.00</p> }
+                { pair.pair && <p><span>Currency Pair: </span>{ pair.pair }</p> }
+                { pair.lotSize && <p><span>Lot Size: </span>{ pair.lotSize.toLocaleString() }.00</p> }
                 { pair.position && <p><span>Position: </span>{ pair.position }</p> }
                 { pair.openedAt && <p><span>Opened At: </span>{ pair.openedAt.toFixed(4) }</p> }
                 { pair.closedAt && <p><span>Closed At: </span>{ pair.closedAt.toFixed(4) }</p> }
