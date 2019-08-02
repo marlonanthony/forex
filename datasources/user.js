01f04248580f0a52759477436b8087f9b5dcb340
marlonanthony <mad1083@gmail.com>
2019-08-02 16:59:00 -0400

    refactored user.js exitPosition message

--- datasources/user.js
+++ datasources/user.js
@@ -94,7 +94,7 @@ class UserAPI extends DataSource {
       await user.save() 
 
       const success = true 
-      const message = `${savedPair.profitLoss > 0 ? `Congrats! ` : ''}${user.name} you've closed your ${savedPair.position} position on ${savedPair.pair} at ${closedAt}${savedPair.profitLoss > 0 ? '! For a profit of '+Math.round(savedPair.profitLoss) : '. For a loss of '+Math.round(savedPair.profitLoss)}`
+      const message = `${savedPair.profitLoss > 0 ? `Congrats! ` : ''}${user.name} you've closed your ${savedPair.position} position on ${savedPair.pair} at ${closedAt}${savedPair.profitLoss > 0 ? '! For a profit of '+Math.round(savedPair.profitLoss)+'!' : '. For a loss of '+Math.round(savedPair.profitLoss)+'.'}`
       return { success, message, pair: savedPair }
     }
     catch (error) { throw error }
