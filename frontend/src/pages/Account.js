7c584bfc4f7af288eb7ae3c7fc2cd5f69b541c65
marlonanthony <mad1083@gmail.com>
2019-08-04 03:09:53 -0400

    refactored Account.js

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -9,7 +9,10 @@ import AddFunds from '../components/AddFunds'
 
 const Account = props => {
   const [open, setOpen] = useState(true),
-        userData = useQuery(MEQUERY)
+        user = useQuery(MEQUERY)
+
+  if(user.loading) return <p>Loading...</p>
+  if(user.error) return <Redirect to='/login' />
   
   return (
     <Query query={ GETPAIRS }>
@@ -25,11 +28,11 @@ const Account = props => {
         } 
       })
 
-      return (
+      return user.data && (
         <main>
-          <h2>{ userData.data && userData.data.me.name }</h2>
+          <h2>{ user.data.me.name }</h2>
           <div>
-            <p><span>Available Balance: </span>{ userData.data && userData.data.me.bankroll.toLocaleString() }.00</p>
+            <p><span>Available Balance: </span>{ user.data.me.bankroll.toLocaleString() }.00</p>
             <p><span>Total P/L: </span>{ count }</p>
             <AddFunds />
           </div>
@@ -37,7 +40,7 @@ const Account = props => {
           { props.location.state &&  (
             <div>
               <h3>New Position</h3>
-              <div className='pair_divs'>
+              <div className='pair_divs' style={{ textAlign: 'center' }}>
                 <p><span>Pair: </span>{ props.location.state.data.openPosition.pair.pair }</p>
                 <p><span>Lot Size: </span>{ props.location.state.data.openPosition.pair.lotSize.toLocaleString() }.00</p>
                 <p><span>Pip Dif: </span>{ props.location.state.data.openPosition.pair.openedAt }</p>
@@ -51,8 +54,8 @@ const Account = props => {
           <button onClick={() => setOpen(false)}>closed</button>
           <div>
           { data.getPairs && data.getPairs.map(pair => pair.open && open && (
-            <div className='pair_divs' key={pair.id}>
-              <Link to={{ pathname: '/pair', state: { pair, me: userData.data && userData.data.me } }}>
+            <div className='pair_divs' key={ pair.id }>
+              <Link to={{ pathname: '/pair', state: { pair, me: user.data.me } }}>
                 { pair.pair && <p><span>Currency Pair: </span>{ pair.pair }</p> }
                 { pair.lotSize && <p><span>Lot Size: </span>{ pair.lotSize.toLocaleString() }.00</p> }
                 { pair.position && <p><span>Position: </span>{ pair.position }</p> }
