e1ea581ff8387961d0ede59a6181fa1f44eb97e2
marlonanthony <mad1083@gmail.com>
2019-08-07 00:58:57 -0400

    replaced render prop with hook in Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,50 +1,47 @@
 import React, { useState } from 'react'
-import { Mutation } from 'react-apollo'
+import { useMutation } from 'react-apollo'
 
 import { REGISTERMUTATION } from '../../graphql/mutations/register'
 
 export default function Register(props) {
   const [email, setEmail] = useState(''),
         [password, setPassword] = useState(''),
-        [name, setName] = useState('') 
-        
+        [name, setName] = useState(''),
+        [register, { error }] = useMutation(REGISTERMUTATION)
+
   return (
-    <Mutation mutation={REGISTERMUTATION}>
-      {(register, { error }) => ( 
-        <div className='register'>
-          <form onSubmit={ async e => {
-            e.preventDefault()
-            await register({ variables: { email, password, name } })
-            props.history.push('/login')
-          }}>
-            <h2>Sign Up</h2>
-            <input
-              required
-              name='email'
-              type='email'
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder='Enter your email'
-            />
-            <input
-              required
-              type='password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder='Enter your password'
-            />
-            <input
-              required
-              type='text'
-              value={name}
-              onChange={e => setName(e.target.value)}
-              placeholder='Enter your name'
-            />
-            { error && <p>{ error.message }</p> }
-            <button type='submit' >SignUp</button>
-          </form>
-        </div>
-      )}
-    </Mutation>
+    <div className='register'>
+      <form onSubmit={ async e => {
+        e.preventDefault()
+        await register({ variables: { email, password, name } })
+        props.history.push('/login')
+      }}>
+        <h2>Sign Up</h2>
+        <input
+          required
+          name='email'
+          type='email'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder='Enter your email'
+        />
+        <input
+          required
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder='Enter your password'
+        />
+        <input
+          required
+          type='text'
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder='Enter your name'
+        />
+        { error && <p>{ error.message }</p> }
+        <button type='submit' >SignUp</button>
+      </form>
+    </div>
   )
 }
