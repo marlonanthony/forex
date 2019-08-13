2dade3746d65c8065a986610b22c577081f805bc
marlonanthony <mad1083@gmail.com>
2019-08-13 16:15:56 -0400

    deploy

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ const server = new ApolloServer({
   }
 })
 
+app.use(express.static('public'))
+
 app.use(session({
   secret,
   resave: false,
   saveUninitialized: false
 }))
-
-app.use(express.static('public'))
 
 app.get('*', (req, res) => {
   res.send(path.resolve(__dirname, 'public', 'index.html'))
