cdc97f269f7489f8329eea6cb7cef606031b8868
marlonanthony <mad1083@gmail.com>
2019-08-16 23:42:23 -0400

    deploy

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  alphaVantageAPIKey: 'WD41TD2JDPAUOAJH',
+  mongoPassword: 'marlon123456',
+  secret: 'muahahahhaaahaahaahhaaha'
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+  alphaVantageAPIKey: process.env.alphaVantageAPIKey,
+  mongoPassword: process.env.mongoPassword,
+  secret: process.env.secret
+}
